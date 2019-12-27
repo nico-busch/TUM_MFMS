63cdcb9309cd076a53c2db76644b1a615ba4b6a1
Nicolas Busch <42951229+nico089@users.noreply.github.com>
2019-12-27 22:38:20 +0100

    taxi zones viz

--- zones.py
+++ zones.py
@@ -1,16 +1,32 @@
 import geopandas as gpd
 from bokeh.plotting import figure, show
-from bokeh.models import GeoJSONDataSource
+from bokeh.models import GeoJSONDataSource, LogColorMapper
+from bokeh.palettes import Inferno10 as palette
+from bokeh.tile_providers import get_provider, Vendors
+import pandas as pd
+
+df = pd.read_pickle('input.pkl')
+df = df.groupby(['PULocationID']).agg({('ground_travel_time', 'count'): 'sum'})
+df.columns = ['count']
 
 gdf = gpd.read_file('data/taxi_zones/taxi_zones.shp')
 gdf = gdf.set_index('OBJECTID')
-gdf = gdf.to_crs({'init': 'epsg:32118'})
+gdf = gdf.to_crs({'init': 'epsg:3857'})
+gdf = gdf.join(df, how='inner')
 
 source = GeoJSONDataSource(geojson=gdf.to_json())
-p = figure(title='test',
-           x_axis_label='x', y_axis_label='y',
-           tooltips=[('Borough', '@borough'), ('Zone', '@zone')])
+palette.reverse()
+tile_provider = get_provider(Vendors.CARTODBPOSITRON_RETINA)
+color_mapper = LogColorMapper(palette=palette)
+p = figure(title='New York Taxi Pickups',
+           x_axis_location=None, y_axis_location=None,
+           tooltips=[('Borough', '@borough'), ('Zone', '@zone'), ('Pickups', '@count')])
+p.add_tile(tile_provider)
 
-zones = p.patches('xs', 'ys', source=source, line_color="white", line_width=0.5)
+zones = p.patches('xs', 'ys', source=source,
+                  line_color="white", line_width=0.5,
+                  fill_color={'field': 'count', 'transform': color_mapper},
+                  alpha=0.75)
+p.grid.grid_line_color = None
 p.hover.point_policy = "follow_mouse"
 show(p)
