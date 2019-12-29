2ba71f86b3323f5064b6abd6c341a7e0691e51bc
Nicolas Busch <42951229+nico089@users.noreply.github.com>
2019-12-29 11:38:13 +0100

    more viz

--- zones.py
+++ zones.py
@@ -1,30 +1,33 @@
 import pandas as pd
 import geopandas as gpd
 from bokeh.plotting import figure, show
-from bokeh.models import GeoJSONDataSource, LogColorMapper
-from bokeh.palettes import Cividis10 as palette
+from bokeh.models import GeoJSONDataSource, LinearColorMapper
+from bokeh.palettes import Viridis9 as palette
 from bokeh.tile_providers import get_provider, Vendors
 
 df = pd.read_pickle('input.pkl')
-df = df.groupby(['PULocationID']).agg({('ground_travel_time', 'count'): 'sum'})
-df.columns = ['count']
+pu = df.groupby(['PULocationID']).agg({('ground_travel_time', 'count'): 'sum'})
+pu.columns = ['pu_count']
+do = df.groupby(['DOLocationID']).agg({('ground_travel_time', 'count'): 'sum'})
+do.columns = ['do_count']
 
 gdf = gpd.read_file('https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip')
 gdf = gdf.set_index('OBJECTID')
 gdf = gdf.to_crs({'init': 'epsg:3857'})
-gdf = gdf.join(df, how='inner')
+gdf = gdf.join(pu, how='inner')
+gdf = gdf.join(do, how='inner')
+gdf['sum'] = gdf['pu_count'] + gdf['do_count']
 
 source = GeoJSONDataSource(geojson=gdf.to_json())
 palette.reverse()
-color_mapper = LogColorMapper(palette=palette)
+color_mapper = LinearColorMapper(palette=palette[3:])
 tile_provider = get_provider(Vendors.CARTODBPOSITRON_RETINA)
 p = figure(x_axis_location=None, y_axis_location=None,
-           tooltips=[('Borough', '@borough'), ('Zone', '@zone'), ('Pickups', '@count')])
+           tooltips=[('Borough', '@borough'), ('Zone', '@zone'), ('Pickups', '@pu_count'), ('Dropoffs', '@do_count')])
 p.add_tile(tile_provider)
 zones = p.patches('xs', 'ys', source=source,
                   line_color="white", line_width=0.5,
-                  fill_color={'field': 'count', 'transform': color_mapper},
+                  fill_color={'field': 'sum', 'transform': color_mapper},
                   alpha=0.75)
-p.grid.grid_line_color = None
 p.hover.point_policy = "follow_mouse"
 show(p)
