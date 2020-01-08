749c1a9b8614a2654a758673fc91ce2ed10aa154
Nicolas Busch <42951229+nico089@users.noreply.github.com>
2020-01-08 11:40:54 +0100

    model b implemented (wrong obj value)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import models
 import viz
@@ -6,13 +7,19 @@ df = pd.read_pickle('data/trips_ny.pkl')
 gdf = pd.read_pickle('data/zones_ny.pkl')
 
 # data pruning
-n_zones = 25
+n_zones = 10
 pu = (df['ground_travel_time'] * df['n_trips']).groupby(['pickup_location']).sum()
 do = (df['ground_travel_time'] * df['n_trips']).groupby(['dropoff_location']).sum()
 zones = (pu + do).nlargest(n_zones).index.sort_values()
 df = df.loc[(zones, zones), :]
 df.index = df.index.remove_unused_levels()
+df = df.reindex(pd.MultiIndex.from_product([zones, zones]), fill_value=0)
 
-df, hubs, obj = models.model_a(df, 5, alpha=5/60, beta=1)
-if hubs.size > 0:
-    viz.viz_hubs(df, gdf, hubs)
+df1, hubs1, obj1 = models.model_a(df, 5, 0, beta=1)
+df2, hubs2, obj2 = models.model_b(df, 5, 0, beta=1)
+
+print(hubs1, obj1)
+print(hubs2, obj2)
+
+# if hubs.size > 0:
+#     viz.viz_hubs(df, gdf, hubs)
--- models.py
+++ models.py
@@ -7,7 +7,6 @@ def model_a(df, p, alpha=0, beta=1):
     # parameter preparation
     routes = df.index
     zones = df.index.get_level_values(0).unique()
-    n_zones = zones.size
     shape = routes.levshape
     g = (df['ground_travel_time'] / pd.to_timedelta(1, 'h')).to_numpy().reshape(shape)
     a = (df['air_travel_time'] / pd.to_timedelta(1, 'h')).to_numpy().reshape(shape)
@@ -20,18 +19,17 @@ def model_a(df, p, alpha=0, beta=1):
 
     # model
     model = Model()
-    x = model.addVars(routes, routes, obj=x_coeff, vtype=GRB.BINARY)
-    y = model.addVars(routes, obj=y_coeff, vtype=GRB.BINARY)
+    model.setParam('TimeLimit', 1800)
+    x = model.addVars(routes, routes, obj=x_coeff)
+    y = model.addVars(routes, obj=y_coeff)
     z = model.addVars(zones, vtype=GRB.BINARY)
     model.modelSense = GRB.MINIMIZE
 
     # constraints
     model.addConstr(z.sum() <= p)
     model.addConstrs(x.sum(i, j, '*', '*') + y[i, j] == 1 for i, j in routes)
-    # model.addConstrs(x[i, j, k, l] <= z[k] for i, j in routes for k, l in routes)
-    # model.addConstrs(x[i, j, k, l] <= z[l] for i, j in routes for k, l in routes)
-    model.addConstrs(x.sum('*', '*', k, '*') <= n_zones * (n_zones - p + 1) * z[k] for k in zones)
-    model.addConstrs(x.sum('*', '*', '*', l) <= n_zones * (n_zones - p + 1) * z[l] for l in zones)
+    model.addConstrs(x.sum(i, j, k, '*') <= z[k] for i, j in routes for k in zones)
+    model.addConstrs(x.sum(i, j, '*', l) <= z[l] for i, j in routes for l in zones)
 
     # optimize
     model.optimize()
@@ -41,6 +39,51 @@ def model_a(df, p, alpha=0, beta=1):
     trips_air.index.names = df.index.names
     df = df.join(trips_air, how='left')
     hubs = np.array([k for k, v in z.items() if v.X == 1])
+    obj = model.getObjective().getValue()
+
+    return df, hubs, obj
+
+
+def model_b(df, p, alpha=0, beta=1):
+
+    # parameter preparation
+    routes = df.index
+    zones = df.index.get_level_values(0).unique()
+    n_zones = zones.size
+    shape = routes.levshape
+    g = (df['ground_travel_time'] / pd.to_timedelta(1, 'h')).to_numpy().reshape(shape)
+    a = (df['air_travel_time'] / pd.to_timedelta(1, 'h')).to_numpy().reshape(shape)
+    d = df['n_trips'].to_numpy().reshape(shape)
+
+    # coefficients
+    x_coeff = np.tile(g, (n_zones, 1, 1))
+    y_coeff = np.tile(a, (n_zones, 1, 1))
+    z_coeff = g
+    w_coeff = g
+
+    # model
+    model = Model()
+    model.setParam('TimeLimit', 1800)
+    x = model.addVars(zones, routes, obj=x_coeff)
+    y = model.addVars(zones, routes, obj=y_coeff)
+    z = model.addVars(routes, obj=z_coeff)
+    w = model.addVars(routes, obj=w_coeff)
+    h = model.addVars(zones, vtype=GRB.BINARY)
+    model.modelSense = GRB.MINIMIZE
+
+    # constraints
+    model.addConstr(h.sum() <= p)
+    model.addConstrs(z.sum(i, '*') + w.sum(i, '*') == np.sum(d[zones.get_loc(i)]) for i in zones)
+    model.addConstrs(x.sum(i, '*', j) + w[i, j] == d[zones.get_loc(i), zones.get_loc(j)] for i, j in routes)
+    model.addConstrs(y.sum(i, k, '*') + x.sum(i, k, '*') - y.sum(i, '*', k) - z[i, k] == 0 for i, k in routes)
+    model.addConstrs(z[i, k] <= np.sum(d[zones.get_loc(i)]) * h[k] for i, k in routes)
+    model.addConstrs(x.sum('*', l, j) <= np.sum(d[:, zones.get_loc(j)]) * h[l] for l, j in routes)
+
+    # optimize
+    model.optimize()
+
+    # solution
+    hubs = np.array([k for k, v in h.items() if v.X == 1])
     obj = model.getObjective().getValue()
 
     return df, hubs, obj
