cb724bbf5dacf08fabdd07bef08b0807890dcf4a
Nicolas Busch <42951229+nico089@users.noreply.github.com>
2020-03-26 20:30:45 +0100

    prepared for submission

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ import plots
 import viz
 import input
 
+# df, gdf = input.input_ny()
+# df, gdf = input.input_chicago()
+
 df = pd.read_pickle('data/trips_ny.pkl')
 gdf = pd.read_pickle('data/zones_ny.pkl')
 
 # df = pd.read_pickle('data/trips_chicago.pkl')
 # gdf = pd.read_pickle('data/zones_chicago.pkl')
 
-# study.sensitivity_analysis(df).to_pickle('results/sensitivity_analysis.pkl')
-# study.gurobi_vs_ga(df).to_pickle('results/gurobi_vs_ga.pkl')
+viz.viz_zones(df, gdf)
 
-# results = pd.read_pickle('results/gurobi_vs_ga.pkl')
+study.sensitivity_analysis(df).to_pickle('results/sensitivity_analysis.pkl')
+study.gurobi_vs_ga(df).to_pickle('results/gurobi_vs_ga.pkl')
 
-# plots.plot_gurobi_vs_ga(results)
-# plots.plot_travel_time(results)
+results_sensitivity = pd.read_pickle('results/sensitivity_analysis.pkl')
+results_gurobi = pd.read_pickle('results/gurobi_vs_ga.pkl')
 
-# viz.viz_zones(df, gdf)
+plots.plot_travel_time(results_sensitivity)
+plots.plot_gurobi_vs_ga(results_gurobi)
 
-obj, hubs, trips = models.model_a_ga(df, 20, alpha=5/60)
-viz.viz_hubs(df, gdf, hubs)
+# obj, hubs, trips = models.model_a_ga(df, 10, alpha=5/60)
+# viz.viz_savings(df, gdf, hubs, trips)
 
--- viz.py
+++ viz.py
@@ -1,12 +1,8 @@
 import pandas as pd
-import numpy as np
-import models
 from bokeh.io import output_file
 from bokeh.plotting import figure, show
-from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter, HoverTool
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, LogColorMapper, ColorBar, NumeralTickFormatter, HoverTool
 from bokeh.tile_providers import get_provider, Vendors
-import seaborn as sns
-import matplotlib.pyplot as plt # due to release problems older version is needed -> downgrade to 3.1.0
 
 def viz_zones(df, gdf):
 
@@ -39,20 +35,22 @@ def viz_zones(df, gdf):
     hover = HoverTool(renderers=[zones],
                       tooltips=[('Borough', '@borough'),
                                 ('Zone', '@zone'),
-                                ('Pickup Vehicle Hours', '@vehicle_hrs{int}')])
+                                ('Total Vehicle Hours', '@vehicle_hrs{int}')])
     p.add_tools(hover)
     p.add_layout(color_bar, 'below')
     p.hover.point_policy = "follow_mouse"
 
     show(p)
 
-def viz_hubs(df, gdf, hubs):
+def viz_hubs(gdf, hubs, trips):
 
     output_file('results/hubs.html')
 
-    pu = (df['ground_travel_time'] * df['n_trips']).groupby(['pickup_location']).sum()
-    do = (df['ground_travel_time'] * df['n_trips']).groupby(['dropoff_location']).sum()
-    vehicle_hrs = ((pu + do) / pd.to_timedelta(1, 'h')).rename('vehicle_hrs')
+    pu = (trips.loc[trips['hubs'].notna(), 'travel_time']
+          * trips.loc[trips['hubs'].notna(), 'n_trips']).groupby(['pickup_location']).sum()
+    do = (trips.loc[trips['hubs'].notna(), 'travel_time']
+          * trips.loc[trips['hubs'].notna(), 'n_trips']).groupby(['dropoff_location']).sum()
+    vehicle_hrs = (pu.add(do, fill_value=0) / pd.to_timedelta(1, 'h')).rename('vehicle_hrs')
 
     gdf = gdf.to_crs({'init': 'epsg:3857'})
     gdf = gdf.join(vehicle_hrs, how='right')
@@ -60,11 +58,10 @@ def viz_hubs(df, gdf, hubs):
     gdf_hubs['geometry'] = gdf_hubs.centroid
     gdf = gdf.explode()
 
+    palette = linear_gradient('#bfe1ff', '#0065bd', n=50) + linear_gradient('#0065bd', '#00335f', n=200)
     source_zones = GeoJSONDataSource(geojson=gdf.to_json())
     source_hubs = GeoJSONDataSource(geojson=gdf_hubs.to_json())
-    from bokeh.palettes import Viridis256 as palette
-    palette.reverse()
-    color_mapper = LinearColorMapper(palette=palette[50:], low=gdf['vehicle_hrs'].min(), high=gdf['vehicle_hrs'].max())
+    color_mapper = LinearColorMapper(palette=palette, low=vehicle_hrs.min(), high=vehicle_hrs.max())
     tile_provider = get_provider(Vendors.CARTODBPOSITRON_RETINA)
     p = figure(x_axis_location=None, y_axis_location=None)
     p.add_tile(tile_provider)
@@ -81,235 +78,70 @@ def viz_hubs(df, gdf, hubs):
     hover = HoverTool(renderers=[zones],
                       tooltips=[('Borough', '@borough'),
                                 ('Zone', '@zone'),
-                                ('Zone', '@zone'),
-                                ('Total Vehicle Hours', '@vehicle_hrs{int}')])
+                                ('Air Travel Vehicle Hours', '@vehicle_hrs{int}')])
     p.add_tools(hover)
     p.add_layout(color_bar, 'below')
     p.hover.point_policy = "follow_mouse"
 
     show(p)
 
-def viz_connection_heatmap(n_hubs, n_zones=263, alpha=0, beta=1):
-    # run Genetic Algorithm
-    data_ = prepare_data(n_zones)
-    _, _, df = models.model_a_ga(data_, n_hubs,  alpha=alpha, beta=beta)
+def hex_to_RGB(hex):
+    return [int(hex[i:i + 2], 16) for i in range(1, 6, 2)]
 
-    # create data
-    df_hubs = trip_count_df(df)
+def RGB_to_hex(RGB):
+    RGB = [int(x) for x in RGB]
+    return "#" + "".join(["0{0:x}".format(v) if v < 16 else "{0:x}".format(v) for v in RGB])
 
-    # plot heatmap
-    df_ = df_hubs.copy()
-    df_['trip_count'] = (df_['trip_count'] / 1000).astype(int)
-    df_ = df_.reset_index().pivot(columns='destination', index='origin', values='trip_count')
-    df_ = df_.fillna(0)
-    df_ = df_.astype(int)
-    mask = np.ones_like(df_)
+def linear_gradient(start_hex, finish_hex="#FFFFFF", n=10):
+    s = hex_to_RGB(start_hex)
+    f = hex_to_RGB(finish_hex)
+    hex_list = [start_hex]
+    for t in range(1, n):
+        curr_vector = [int(s[j] + (float(t) / (n - 1)) * (f[j] - s[j])) for j in range(3)]
+        hex_list.append(RGB_to_hex(curr_vector))
+    return hex_list
 
-    mask[np.tril_indices_from(df_)] = False
+def viz_savings(df, gdf, hubs, trips):
 
-    fig, ax = plt.subplots(1, 1, figsize = (15, 15))
-    sns.heatmap(df_, cmap="YlGnBu", annot=True, fmt='', mask=mask, square=True, linewidths=1,
-                cbar_kws={'label': 'Number of trips [k]'})
-    ax.set_ylabel('')
-    ax.set_xlabel('')
-    plt.show()
+    output_file('results/hubs.html')
 
-def viz_hub_importance(n_hubs, n_zones=263, alpha=0, beta=1):
-    # run Genetic Algorithm
-    data_ = prepare_data(n_zones)
-    _, hubs, df = models.model_a_ga(data_, n_hubs,  alpha=alpha, beta=beta)
+    temp = (df['ground_travel_time'] - trips['travel_time']).loc[trips['hubs'].notna()]
+    temp[temp > pd.to_timedelta(3, 'h')] = pd.to_timedelta(0, 'h')
+    temp = temp * df['n_trips']
+    diff = (temp.groupby(['pickup_location']).sum() + temp.groupby(['dropoff_location']).sum()) \
+           / (df['n_trips'].groupby(['pickup_location']).sum() + df['n_trips'].groupby(['dropoff_location']).sum())
+    savings = (diff / pd.to_timedelta(1, 'm')).rename('savings')
 
-    # create data
-    df_hubs = trip_count_df(df)
-    df_ = hub_importance(hubs, df_hubs)
+    gdf = gdf.to_crs({'init': 'epsg:3857'})
+    gdf = gdf.join(savings, how='right')
+    gdf_hubs = gdf.loc[hubs]
+    gdf_hubs['geometry'] = gdf_hubs.centroid
+    gdf = gdf.explode()
 
-    # donut plot
-    fig, ax = plt.subplots(1, 1, figsize=(15, 15))
-    df_ = df_.drop(columns='hubs_')
-    ax = df_.plot.bar(rot=0, cmap="Set3")
-    ax.set_ylabel('Number of trips per month [k]')
-    ax.set_xlabel('')
-    ax.get_legend().remove()
-    plt.show()
+    # palette = linear_gradient('#bfe1ff', '#0065bd', n=50) + linear_gradient('#0065bd', '#00335f', n=200)
+    palette = linear_gradient('#bfe1ff', '#00335f', n=256)
+    source_zones = GeoJSONDataSource(geojson=gdf.to_json())
+    source_hubs = GeoJSONDataSource(geojson=gdf_hubs.to_json())
+    color_mapper = LogColorMapper(palette=palette, low=savings.min(), high=savings.max())
+    tile_provider = get_provider(Vendors.CARTODBPOSITRON_RETINA)
+    p = figure(x_axis_location=None, y_axis_location=None)
+    p.add_tile(tile_provider)
+    zones = p.patches('xs', 'ys', source=source_zones,
+                      line_color="white", line_width=0.5,
+                      fill_color={'field': 'savings', 'transform': color_mapper},
+                      alpha=0.75)
+    p.circle('x', 'y', source=source_hubs, size=10, color="darkorange")
+    color_bar = ColorBar(color_mapper=color_mapper,
+                         location=(0, 0),
+                         orientation='horizontal',
+                         major_tick_line_color=None,
+                         formatter=NumeralTickFormatter(format='0a'))
+    hover = HoverTool(renderers=[zones],
+                      tooltips=[('Borough', '@borough'),
+                                ('Zone', '@zone'),
+                                ('Air Travel Savings', '@savings')])
+    p.add_tools(hover)
+    p.add_layout(color_bar, 'below')
+    p.hover.point_policy = "follow_mouse"
 
-    '''
-    fig, ax = plt.subplots(1, 1, figsize = (15, 15))
-    circle = plt.Circle((0, 0), 0.7, color='white')
-    ax.pie(df_['trip_count'], labels=df_['hubs_'], wedgeprops={'linewidth': 7, 'edgecolor': 'white'})
-    ax.add_artist(circle)
-    #axs[k, l].set_title('Number of hubs: ' + str(h_[count_ - 1]))
-    '''
-    plt.show()
-
-def viz_hub_utilization(n_hubs, n_zones=263, alpha=0, beta=1):
-
-    data_ = prepare_data(n_zones)
-
-    df_total = pd.DataFrame(columns=['trips_per_hub', 'trip_type', 'trip_count', 'p', 'percentage'])
-    df_total_2 = pd.DataFrame(columns=['p', 'trips_per_hub'])
-
-    for i in range(n_hubs + 1):
-        if (i >= 2):
-
-            # run Genetic Algorithm
-            _, hubs, df = models.model_a_ga(data_, i, alpha=alpha, beta=beta)
-
-            # adding trip transfer types information to df
-            df['trip_type'] = 'non_air'
-            for index, row in df.dropna().iterrows():
-                tmp = tuple(row['hubs'])
-                if ((tmp[0] == index[0] and tmp[1] == index[1]) or (tmp[0] == index[1] and tmp[1] == index[0])):
-                    df.loc[index, 'trip_type'] = 'direct'
-                elif (tmp[0] == index[0] or tmp[1] == index[1] or tmp[1] == index[0] or tmp[0] == index[1]):
-                    df.loc[index, 'trip_type'] = 'one_transfer'
-                else:
-                    df.loc[index, 'trip_type'] = 'two_transfer'
-
-            un = np.array(['direct', 'one_transfer', 'two_transfer'])
-            df_ = pd.DataFrame(data=un.flatten(), columns=['trip_type'])
-            df_['trip_count'] = np.NAN
-            df_['p'] = i
-            df_['percentage'] = df[df['trip_type'] != 'non_air']['n_trips'].sum()
-            count = 0
-            for ii in un:
-                df_.loc[count, 'trip_count'] = df[df['trip_type'] == ii]['n_trips'].sum()
-                df_.loc[count, 'percentage'] = df_.loc[count, 'trip_count'] / df_.loc[count, 'percentage']
-                df_.loc[count, 'trips_per_hub'] = df[df['trip_type'] != 'non_air']['n_trips'].sum() / i
-                count = count + 1
-            df_total = df_total.append(df_, ignore_index=True)
-
-
-            tph = df[df['trip_type'] != 'non_air']['n_trips'].sum() / i
-            df2 = pd.DataFrame([[i, tph]], columns=['p', 'trips_per_hub'])
-            df_total_2 = df_total_2.append(df2, ignore_index=True)
-
-
-    # plot
-
-    print(df_total)
-
-    colors = ["#006D2C", "#31A354", "#74C476"]
-    pivot_df = df_total.pivot(index='p', columns='trip_type', values='trip_count')
-    pivot_df.plot.bar(stacked=True, color=colors)
-
-    df_total_2 = df_total_2.set_index('p')
-    df_total_2.plot(style='.-')
-
-    plt.show()
-
-def viz_time_savings_histogram(n_hubs=[3,5,7,10], n_zones=263, alpha=5/60, beta=1):
-    # data preperation
-    data = pd.read_pickle('data/trips_ny.pkl')
-    total = (data['ground_travel_time'] * data['n_trips']).groupby(['pickup_location']).sum() + \
-            (data['ground_travel_time'] * data['n_trips']).groupby(['dropoff_location']).sum()
-    zones = total.nlargest(n_zones).index.sort_values()
-    data_ = data.loc[(zones, zones), :]
-    data_.index = data_.index.remove_unused_levels()
-    data_ = data_.reindex(pd.MultiIndex.from_product([zones, zones], names=data_.index.names), fill_value=0)
-
-    '''
-    # Only uncomment if new data calculation is wanted (~20min)
-    
-    for i in n_hubs:
-        _, hubs, df = models.model_a_ga(data_, i, alpha=alpha, beta=beta)
-        temp = (data_['ground_travel_time'] - df['travel_time']).loc[df['hubs'].notna()]
-        temp[temp > pd.to_timedelta(3, 'h')] = pd.to_timedelta(0, 'h')
-        temp[temp <= pd.to_timedelta(0, 'h')] = np.NaN
-        temp = temp.dropna()
-        savings = (temp / pd.to_timedelta(1, 'm')).rename('savings')
-        savings.to_frame().to_pickle('results/histogram_data_'+str(i)+'_hubs.pkl')
-    '''
-
-    # histogram
-    ncols = 2
-    nrows = 2
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
-    counter = 0
-    title_c = 0
-    for i in range(nrows):
-        for j in range(ncols):
-            ax = axes[i][j]
-            df = pd.read_pickle('results/histogram_data_' + str(n_hubs[counter]) + '_hubs.pkl')
-            # Plot when we have data
-            if counter < 6:
-                df = df.merge(data_['n_trips'], how='left', left_index=True, right_index=True)
-                ax.hist(df['savings'], color='skyblue', alpha=0.8, label='{}'.format(n_hubs[counter]), bins=range(0,130,2), weights=df.n_trips)
-                ax.set_xlabel('Time savings [min]')
-                ax.set_ylabel('Number of trips')
-                ax.set_title('' + str(n_hubs[title_c]) + ' hubs')
-                title_c = title_c + 1
-                ax.set_ylim([0, 500000])
-                ax.set_xlim([0, 60])
-                start, end = ax.get_xlim()
-                ax.xaxis.set_ticks(np.arange(0, end, 10))
-            # Remove axis when we no longer have data
-            else:
-                ax.set_axis_off()
-            counter += 1
-    fig.tight_layout()
-    plt.show()
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Supportive functions
-
-def prepare_data(n_zones):
-    # data preperation
-    data = pd.read_pickle('data/trips_ny.pkl')
-    total = (data['ground_travel_time'] * data['n_trips']).groupby(['pickup_location']).sum() + \
-            (data['ground_travel_time'] * data['n_trips']).groupby(['dropoff_location']).sum()
-    zones = total.nlargest(n_zones).index.sort_values()
-    data_ = data.loc[(zones, zones), :]
-    data_.index = data_.index.remove_unused_levels()
-    return data_.reindex(pd.MultiIndex.from_product([zones, zones], names=data_.index.names), fill_value=0)
-
-def trip_count_df(df):
-    # create df_hubs for counting all trips going through a hub connection
-    un = df['hubs'].unique()
-    un = np.delete(un, 0)  # delete nan value
-    df_hubs = pd.DataFrame(data=un.flatten(), columns=['hubs'])
-    df_hubs = df_hubs.set_index('hubs')
-    df_hubs['trip_count'] = 0
-    for index, row in df_hubs.iterrows():
-        df_hubs.loc[index, 'trip_count'] = df.loc[df['hubs'] == tuple(index)]['n_trips'].sum()
-    df_hubs.index = pd.MultiIndex.from_tuples(df_hubs.index, names=('origin', 'destination'))
-    df_hubs.index = df_hubs.index.drop_duplicates(keep='first')
-
-    # aggregate both demand direction of a hub connection to one single value
-    tmp = df_hubs.copy()
-    existing_indexes = []
-    for index, row in df_hubs.iterrows():
-        df_hubs.loc[index] = tmp.loc[index] + tmp.loc[(index[1], index[0])] + 0
-    return df_hubs
-
-def hub_importance(hubs, df_hubs):
-    # create dataframe representing the hub importance (number of trips)
-    df_ = pd.DataFrame(data=hubs.flatten(), columns=['hubs'])
-    df_['trip_count'] = 0
-    df_['hubs_'] = df_['hubs']
-    df_ = df_.set_index('hubs')
-    for i in hubs:
-        for j in hubs:
-            if (i != j):
-                df_.loc[(i), 'trip_count'] = df_.loc[(i), 'trip_count'] + df_hubs.loc[(i, j), 'trip_count']
-
-    df_['trip_count'] = df_['trip_count'] / 1000
-    return df_.sort_values(by=['trip_count'])
-
-
-
-'''
-
-    Kann man löschen
-    
-    bins = range(0,130,10)
-    #labels = range(0,130,2)
-    df['binned_3_hubs'] = pd.cut(df['3_hubs'], bins=bins)
-
-    df['binned_3_hubs'] = df['binned_3_hubs'].to_frame().applymap(str)
-
-    plt.bar(df['binned_3_hubs'],df['n_trips'])
-    plt.show()
-
-    print(df)
-    exit()
-'''
+    show(p)
